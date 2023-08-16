9d1858028dfbb88b8ba3335dfc02e5af6af6d09c
franciscolara1 <fra.larat@duocuc.cl>
2023-08-16 18:35:06 -0400

    Borrar

--- resources/views/páginas/login.blade.php
+++ resources/views/páginas/login.blade.php
@@ -2,17 +2,5 @@
 
 @section('login')
 
-<h2>Aca se crea un Login</h2>
-<h3>  aaaaaaaaaaaaaaaa</h3>
-<h1>eeeeeeeeeeeeeeeeeee</h1>
-
-<div class="container">
-    <form action="">
-        <a href="">hola</a>
-        <a href="">esta</a>
-        <a href="">es</a>
-        <a href="">rama</a>
-    </form>
-</div>
 
 @endsection
