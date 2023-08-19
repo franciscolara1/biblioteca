4ad4d6a1d950595faf2e0f03e6bed36bf0b4c53c
LvngleyRen <aaronarriagada2017@gmail.com>
2023-08-19 14:11:37 -0400

    creacion login

--- resources/views/páginas/login.blade.php
+++ resources/views/páginas/login.blade.php
@@ -2,21 +2,41 @@
 
 
 @section('login')
-    <div >
-        <form >
-            <div >
-                <label for="exampleInputEmail1" class="form-label">Direccion de correo</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css">
+<script src="https://use.fontawesome.com/f59bcd8580.js"></script>
+<div class="container">
+    <div class="row m-5 no-gutters shadow-lg">
+        <div class="col-md-6 d-none d-md-block">
+            <img src="https://images.unsplash.com/photo-1566888596782-c7f41cc184c5?ixlib=rb-1.2.1&auto=format&fit=crop&w=2134&q=80" class="img-fluid" style="min-height:100%;" />
+        </div>
+        <div class="col-md-6 bg-white p-5">
+            <h3 class="pb-3">Login Form</h3>
+            <div class="form-style">
+                <form>
+                    <div class="form-group pb-3">
+                        <input type="email" placeholder="Email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    </div>
+                    <div class="form-group pb-3">
+                        <input type="password" placeholder="Password" class="form-control" id="exampleInputPassword1">
+                    </div>
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div class="d-flex align-items-center"><input name="" type="checkbox" value="" /> <span class="pl-2 font-weight-bold">Remember Me</span></div>
+                        <div><a href="#">Forget Password?</a></div>
+                    </div>
+                    <div class="pb-2">
+                        <button type="submit" class="btn btn-dark w-100 font-weight-bold mt-2">Submit</button>
+                    </div>
+                </form>
+                <div class="sideline">OR</div>
+                <div>
+                    <button type="submit" class="btn btn-primary w-100 font-weight-bold mt-2"><i class="fa fa-facebook" aria-hidden="true"></i> Login With Facebook</button>
+                </div>
+                <div class="pt-4 text-center">
+                    Get Members Benefit. <a href="#">Sign Up</a>
+                </div>
             </div>
-            <div class="col-md-auto">
-                <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-            <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Recordarme</label>
-            </div>
-            <button type="submit" class="btn btn-primary">Iniciar sesion</button>
-        </form>
+        </div>
     </div>
+</div>
+
 @endsection
