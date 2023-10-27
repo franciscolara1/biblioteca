cbff2315e06256d79acb41784c327cde6decbf9a
franciscolara1 <fra.larat@duocuc.cl>
2023-10-27 18:34:30 -0300

    MERGE JORDAN

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LibrosController;
 use App\Http\Controllers\FiltrosController;
+use App\Http\Controllers\AgregarSubscriptorController;
 
 /*
 |--------------------------------------------------------------------------
