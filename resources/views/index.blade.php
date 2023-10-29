95fbe5931e8cba5780a4b5715e09f7fc6d06610a
franciscolara1 <fra.larat@duocuc.cl>
2023-10-29 15:50:03 -0300

    arreglos

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,8 +15,8 @@
     <link href="{{ asset('css/login.css') }}" rel="stylesheet">
     <link href="{{ asset('css/menu.css') }}" rel="stylesheet">
     <link href="{{ asset('css/libros.css') }}" rel="stylesheet">
-
     <link href="{{ asset('css/detalle.css') }}" rel="stylesheet">
+    
     
     <link rel='stylesheet' href='https://cdn-uicons.flaticon.com/uicons-solid-rounded/css/uicons-solid-rounded.css'>
     <!-- Google Fonts -->
